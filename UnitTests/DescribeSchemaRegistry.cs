479cdc8a0914b21031ef186b7a2da44182502d60
noocyte <jarle.nygaard@outlook.com>
2017-04-05 12:11:47 +0200

    Added test

--- UnitTests/DescribeSchemaRegistry.cs
+++ UnitTests/DescribeSchemaRegistry.cs
@@ -14,6 +14,15 @@ namespace UnitTests
             Assert.AreEqual(sut.Definitions.Count, 1);
         }
 
+        [TestMethod]
+        public void ItShouldCreateParameter()
+        {
+            var sut = new SchemaRegistry();
+            var actual = sut.CreateParameter(new SchemaGenerator.Models.ApiParameterDescription("", "query", new SchemaGenerator.Models.ParameterDescriptor(typeof(SomeClass))), sut);
+            Assert.AreEqual(sut.Definitions.Count, 1);
+        }
+
+
         class SomeClass
         {
             public int ANumber { get; set; }
